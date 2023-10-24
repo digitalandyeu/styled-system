f69fc3e78d5c17a7d556db51c13fa3291145fef2
Andrii Iv. (digitalandyeu) <hi@digitalandy.eu>
2023-10-24 06:41:34 +0200

    try: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,34 @@
 import "@/theme/styles/globals.css"
 
 import type { AppProps } from "next/app"
+import Script from "next/script"
 import AppLayout from "@/layouts/AppLayout"
 import { NextPageWithLayout } from "@/pages/page"
 import { ThemeProvider } from "next-themes"
 
-import Gtag from "@/components/Gtag"
 import SEO from "@/components/SEO"
 
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout
 }
 
+const GID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <>
-      <Gtag />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GID}');
+        `}
+      </Script>
       <SEO />
       <ThemeProvider enableSystem={false} attribute="class">
         <AppLayout>{getLayout(<Component {...pageProps} />)}</AppLayout>
